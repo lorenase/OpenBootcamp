dd6b986fb6301d8a1af14e2fd2c20063f33c9baf
Lorena Serrano <lorena.serranoal@gmail.com>
2022-11-26 12:16:46 +0100

    Ejercicios8

---
+++ src/Ejercicios8/Ejercicios8.java
@@ -0,0 +1,19 @@
+package Ejercicios8;
+
+public class Ejercicios8 {
+    public static void main(String[] args){
+        Persona persona = new Persona();
+
+        persona.setEdad(30);
+        int edad = persona.getEdad();
+        System.out.println ("Edad: " + edad);
+
+        persona.setNombre("Juana");
+        String nombre = persona.getNombre();
+        System.out.println ("Nombre: " + nombre);
+
+        persona.setTelefono("+34 600000000");
+        String telefono = persona.getTelefono();
+        System.out.println ("Teléfono: "+ telefono);
+    }
+}
